89f8e17368efa12d0128e3f8cea9921435023433
Vishal Kumar <vishalninaniya8949info@gmail.com>
2025-08-15 15:23:36 +0530

    update envs

--- src/config/passport.js
+++ src/config/passport.js
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      callbackURL: process.env.CALLBACK_URL,
     },
     async (_accessToken, _refreshToken, profile, done) => {
       try {
